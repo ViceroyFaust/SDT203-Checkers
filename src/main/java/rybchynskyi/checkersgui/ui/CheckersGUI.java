c429140ce41812b14e65780861e027f076a7fd35
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2024-05-03 22:44:39 +0300

    Create a working PVP version of CheckersGUI
    Prior to this change, I was still learning how to create a basic GUI
    with JavaFX. I needed to make it so I could play the game with the UI.
    
    This change makes a playable version of the game with the GUI with two
    players.

--- src/main/java/rybchynskyi/checkersgui/ui/CheckersGUI.java
+++ src/main/java/rybchynskyi/checkersgui/ui/CheckersGUI.java
@@ -1,26 +1,45 @@
 package rybchynskyi.checkersgui.ui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import rybchynskyi.checkersgui.core.CheckersLogic;
 import rybchynskyi.checkersgui.core.Coordinate;
 import rybchynskyi.checkersgui.core.Move;
 
-import javax.swing.*;
-
 public class CheckersGUI extends Application {
+    private final double ROOT_PADDING = 12;
     private final double BUTTON_PADDING = 1;
     private final int BUTTON_ROWS = 8;
     private final int BUTTON_COLS = 8;
     private final Button[][] checkerButtons = new Button[BUTTON_ROWS][BUTTON_COLS];
     private final GridPane checkerButtonGridPane = new GridPane();
+    private final GridPane root = new GridPane();
+
+    private final String PLAYER_WHITE_STATUS = "Player O's turn";
+    private final String PLAYER_BLACK_STATUS = "Player X's turn";
+
+    private final String BLACK_WIN = "Player X won!";
+    private final String WHITE_WIN = "Player O won!";
+
+    private final String CHOOSE_PIECE_STATUS = "Please choose a checker";
+    private final String CHOOSE_DESTINATION_STATUS = "Please make a move";
+
+    private Label moveStatusLabel = new Label(CHOOSE_PIECE_STATUS);
+    private Label playerStatusLabel = new Label(PLAYER_BLACK_STATUS);
+
+    private final Alert invalidMoveAlert = new Alert(Alert.AlertType.ERROR, "Error: Illegal move!", ButtonType.OK);
+    private final Alert gameOverAlert = new Alert(Alert.AlertType.CONFIRMATION);
+
     private final CheckersLogic game = new CheckersLogic();
     private Coordinate selectedChecker = null;
 
@@ -44,35 +63,60 @@ public class CheckersGUI extends Application {
                 Button toAdd = new Button();
                 toAdd.setMinSize(32, 32);
                 toAdd.setId(Integer.toString(buttonId));
-                toAdd.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        Button button = (Button) event.getSource();
-                        int id = Integer.parseInt(button.getId());
-                        int row = id / BUTTON_COLS;
-                        int col = id % BUTTON_COLS;
-                        Coordinate currentSelection = new Coordinate(row, col);
-                        System.out.println("Clicked row " + row + ", col " + col);
-                        if (selectedChecker != null) {
-                            Move move = new Move(selectedChecker, currentSelection);
-                            selectedChecker = null;
-                            game.move(move);
-                            updateButtons();
-                        } else {
-                            selectedChecker = currentSelection;
-                        }
-                    }
-                });
+                toAdd.setOnAction(this::handleCheckerButtons);
                 checkerButtons[i][j] = toAdd;
             }
         }
     }
 
-    public void updateButtons() {
+    private void updateButtons() {
         for (int i = 0; i < BUTTON_ROWS; ++i) {
             for (int j = 0; j < BUTTON_COLS; ++j) {
                 checkerButtons[i][j].setText(game.getSymbol(i, j));
             }
+        }
+    }
+
+    private void initRoot() {
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(ROOT_PADDING));
+        root.setVgap(ROOT_PADDING);
+        root.setHgap(ROOT_PADDING);
+
+        root.add(playerStatusLabel, 0, 0);
+        root.add(checkerButtonGridPane, 0, 1);
+        root.add(moveStatusLabel, 0, 2);
+    }
+
+    private void handleCheckerButtons(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        int id = Integer.parseInt(button.getId());
+        int row = id / BUTTON_COLS;
+        int col = id % BUTTON_COLS;
+        Coordinate currentSelection = new Coordinate(row, col);
+        System.out.println("Clicked row " + row + ", col " + col);
+        if (selectedChecker != null) {
+            Move move = new Move(selectedChecker, currentSelection);
+            selectedChecker = null;
+            try {
+                game.move(move);
+            } catch(IllegalArgumentException e) {
+                System.out.println("Illegal move!");
+                invalidMoveAlert.showAndWait();
+            }
+            updateButtons();
+            if (game.isGameOver()) {
+                gameOverAlert.setTitle("Game Over!");
+                gameOverAlert.setContentText(game.getNextPlayerString().equals("X") ? BLACK_WIN : WHITE_WIN);
+                gameOverAlert.showAndWait();
+                Platform.exit();
+                System.exit(0);
+            }
+            moveStatusLabel.setText(CHOOSE_PIECE_STATUS);
+            playerStatusLabel.setText((game.getCurrentPlayerString().equals("X")) ? PLAYER_BLACK_STATUS : PLAYER_WHITE_STATUS);
+        } else {
+            selectedChecker = currentSelection;
+            moveStatusLabel.setText(CHOOSE_DESTINATION_STATUS);
         }
     }
 
@@ -81,13 +125,15 @@ public class CheckersGUI extends Application {
         initCheckerButtons();
         updateButtons();
         initButtonGridPane();
+        initRoot();
 
         stage.setTitle("Checkers");
-        stage.setScene(new Scene(checkerButtonGridPane));
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
+
 }
